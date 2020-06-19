fd3dcf2a3168e50f71f0820495a62be9f52be93e
joseCastro-76 <56211699+joseCastro-76@users.noreply.github.com>
2020-06-18 18:13:37 -0700

    Inital uplad. Adds simple HTTP server. Passes all tests.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code here
+const http = require('http');
+
+const hostname = 'localhost';
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+    console.log(`Received${req.method} request for: ${req.url}`);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+})
+
+server.listen(PORT, hostname, null, () => {
+    console.log(`Server is listening on ${hostname}: ${PORT}`);
+});
